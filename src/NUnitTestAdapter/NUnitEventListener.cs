eb66cedd461789f10b574c65088a2a6ea5316eaf
Terje Sandstrom <terje@hermit.no>
2014-06-15 23:37:18 +0200

    Fixed up for multiple blanks, issue #34

--- src/NUnitTestAdapter/NUnitEventListener.cs
+++ src/NUnitTestAdapter/NUnitEventListener.cs
@@ -94,6 +94,7 @@ namespace NUnit.VisualStudio.TestAdapter
                     : 0;
             if (drop > 0)
                 message = message.Substring(0, length - drop);
+            message = message.Trim();
             if (!string.IsNullOrEmpty(message))
                 testLog.SendMessage(TestMessageLevel.Informational, message);
             string type="";
