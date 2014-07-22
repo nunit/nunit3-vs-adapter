ef1f4b968a02e022b8b97fe50e73d62dc82eed29
Terje Sandstrom <terje@hermit.no>
2014-07-22 13:55:54 +0200

    Added unit tests for checking that the project is set up with correct target framework and correct copy local of Microsoft assembly

---
+++ src/NUnitTestAdapterTests/ProjectTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnit.VisualStudio.TestAdapter.Tests
+{
+    [TestFixture]
+    public class ProjectTests
+    {
+        [Test]
+        public void ThatTheTestAdapterUsesFrameWork35()
+        {
+            var dir = Directory.GetCurrentDirectory();
+            var assembly = Assembly.LoadFrom(dir+"/NUnit.VisualStudio.TestAdapter.dll");
+            var version = assembly.ImageRuntimeVersion;
+            Assert.That(version,Is.EqualTo("v2.0.50727"),"The NUnitTestAdapter project must be set to target .net framework 3.5");
+        }
+
+        [Test]
+        public void ThatNoMSTestDLLIsCopiedToOutput()
+        {
+            var dir = Directory.GetCurrentDirectory();
+            var filesNotToExist = Directory.EnumerateFiles(dir, "Microsoft", SearchOption.TopDirectoryOnly);
+            Assert.IsTrue(!filesNotToExist.Any(),"The reference of NUnitTestAdapter - Microsoft.VisualStudio.TestPlatform.ObjectModel must be set Copy Local to false");
+        }
+    }
+}
