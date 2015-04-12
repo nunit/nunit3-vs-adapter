65b4ff2f527c7d54e922747bbaef5f5d1e11c363
Terje Sandstrom <terje@hermit.no>
2015-04-12 18:41:45 +0200

    fixed up unit tests, and added unit test for correct naming of the adapter

--- src/NUnitTestAdapterTests/ProjectTests.cs
+++ src/NUnitTestAdapterTests/ProjectTests.cs
@@ -15,16 +15,24 @@ namespace NUnit.VisualStudio.TestAdapter.Tests
         [Test]
         public void ThatTheTestAdapterUsesFrameWork35()
         {
-            var dir = Directory.GetCurrentDirectory();
-            var assembly = Assembly.LoadFrom(dir+"/NUnit3TestAdapter.dll");
+            var dir = TestContext.CurrentContext.TestDirectory;
+            var assembly = Assembly.LoadFrom(dir+"/NUnit3.TestAdapter.dll");
             var version = assembly.ImageRuntimeVersion;
             Assert.That(version,Is.EqualTo("v2.0.50727"),"The NUnitTestAdapter project must be set to target .net framework 3.5");
         }
 
         [Test]
+        public void ThatTheTestAdapterEndsWithTestAdapterDll()
+        {
+            var dir = TestContext.CurrentContext.TestDirectory;
+            bool found = File.Exists(dir + "/NUnit3.TestAdapter.dll");
+            Assert.That(found,Is.True,string.Format(@"Did not find 'NUnit3.TestAdapter.dll' in {0}. Ensure the Testadapter ends with '.TestAdapter.dll'",dir));
+        }
+
+        [Test]
         public void ThatNoMSTestDLLIsCopiedToOutput()
         {
-            var dir = Directory.GetCurrentDirectory();
+            var dir = TestContext.CurrentContext.TestDirectory;
             var filesNotToExist = Directory.EnumerateFiles(dir, "Microsoft", SearchOption.TopDirectoryOnly);
             Assert.IsTrue(!filesNotToExist.Any(),"The reference of NUnitTestAdapter - Microsoft.VisualStudio.TestPlatform.ObjectModel must be set Copy Local to false");
         }
@@ -32,8 +40,8 @@ namespace NUnit.VisualStudio.TestAdapter.Tests
         [Test]
         public void ThatTheReferenceToMicrosoftTestObjectModelPointsToVS2012Version()
         {
-            var dir = Directory.GetCurrentDirectory();
-            var assembly = Assembly.LoadFrom(dir + "/NUnit3TestAdapter.dll");
+            var dir = TestContext.CurrentContext.TestDirectory;
+            var assembly = Assembly.LoadFrom(dir + "/NUnit3.TestAdapter.dll");
             var refNames = assembly.GetReferencedAssemblies().Where(ass=>ass.Name=="Microsoft.VisualStudio.TestPlatform.ObjectModel").ToList();
             Assert.IsTrue(refNames != null && refNames.Count() == 1, "No reference to Microsoft.VisualStudio.TestPlatform.ObjectModel found");
             Assert.IsTrue(refNames[0].Version.Major == 11, "Microsoft.VisualStudio.TestPlatform.ObjectModel must point to the 2012 version (11)");
