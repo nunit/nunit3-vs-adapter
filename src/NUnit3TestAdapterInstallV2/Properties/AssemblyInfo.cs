8c0f00ad0e34ef0d44f1ef206de15e34ede210c4
Terje Sandstrom <terje@hermit.no>
2016-10-27 19:52:41 +0200

    Updated name of installer in asselbmyinfo for the installer. Not sure if this is used for anything.

--- src/NUnit3TestAdapterInstallV2/Properties/AssemblyInfo.cs
+++ src/NUnit3TestAdapterInstallV2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NUnit3TestAdapterInstallV2")]
+[assembly: AssemblyTitle("NUnit3TestAdapterInstall")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NUnit3TestAdapterInstallV2")]
+[assembly: AssemblyProduct("NUnit3TestAdapterInstall")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
