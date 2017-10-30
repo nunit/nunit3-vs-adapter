a6a5b4f29356cd1e964c5eefdcd8eaa744c83bc7
Terje Sandstrom <terje@hermit.no>
2017-10-30 20:20:40 +0100

    Updating versions numbers for release 3.9.0

--- src/NUnitTestAdapter/Properties/AssemblyInfo.cs
+++ src/NUnitTestAdapter/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("c0aad5e4-b486-49bc-b3e8-31e01be6fefe")]
-[assembly: AssemblyVersion("3.8.0.0")]
-[assembly: AssemblyFileVersion("3.8.0.0")]
+[assembly: AssemblyVersion("3.9.0.0")]
+[assembly: AssemblyFileVersion("3.9.0.0")]
