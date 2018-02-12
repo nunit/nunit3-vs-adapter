ff6655cd683580f3bb32abf3742af1c435ddc151
jnm2 <me@jnm2.com>
2018-02-12 00:50:26 -0500

    Locate declaring class and state machine class for DiaSession

--- src/NUnitTestAdapter/NavigationDataProvider.cs
+++ src/NUnitTestAdapter/NavigationDataProvider.cs
@@ -22,33 +22,66 @@
 // ***********************************************************************
 
 using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.VisualStudio.TestAdapter.Metadata;
 
 namespace NUnit.VisualStudio.TestAdapter
 {
     public sealed class NavigationDataProvider : IDisposable
     {
-        private readonly DiaSession _session;
+        private readonly string _assemblyPath;
+        private readonly Dictionary<string, DiaSession> _sessionsByAssemblyPath = new Dictionary<string, DiaSession>(StringComparer.OrdinalIgnoreCase);
+#if !NETCOREAPP1_0
+        private readonly IMetadataProvider _metadataProvider;
+#endif
 
         public NavigationDataProvider(string assemblyPath)
         {
             if (string.IsNullOrEmpty(assemblyPath))
                 throw new ArgumentException("Assembly path must be specified.", nameof(assemblyPath));
 
-            _session = new DiaSession(assemblyPath);
-        }
+            _assemblyPath = assemblyPath;
 
-        public NavigationData GetNavigationData(string className, string methodName)
-        {
-            var data = _session.GetNavigationData(className, methodName);
-            if (data == null) return NavigationData.Invalid;
-
-            return new NavigationData(data.FileName, data.MinLineNumber);
+#if !NETCOREAPP1_0
+            _metadataProvider = new ReflectionAppDomainMetadataProvider();
+#endif
         }
 
         public void Dispose()
         {
-            _session.Dispose();
+#if !NETCOREAPP1_0
+            _metadataProvider.Dispose();
+#endif
+            foreach (var session in _sessionsByAssemblyPath.Values)
+                session.Dispose();
+        }
+
+        public NavigationData GetNavigationData(string className, string methodName)
+        {
+            return TryGetSessionData(_assemblyPath, className, methodName)
+#if !NETCOREAPP1_0
+                ?? TryGetSessionData(_metadataProvider.GetStateMachineType(_assemblyPath, className, methodName), "MoveNext")
+                ?? TryGetSessionData(_metadataProvider.GetDeclaringType(_assemblyPath, className, methodName), methodName)
+#endif
+                ?? NavigationData.Invalid;
+        }
+
+        private NavigationData TryGetSessionData(string assemblyPath, string declaringTypeName, string methodName)
+        {
+            if (!_sessionsByAssemblyPath.TryGetValue(assemblyPath, out var session))
+                _sessionsByAssemblyPath.Add(assemblyPath, session = new DiaSession(assemblyPath));
+
+            var data = session.GetNavigationData(declaringTypeName, methodName);
+
+            return data?.FileName == null ? null :
+                new NavigationData(data.FileName, data.MinLineNumber);
+        }
+
+        private NavigationData TryGetSessionData(TypeInfo? declaringType, string methodName)
+        {
+            return declaringType == null ? null :
+                TryGetSessionData(declaringType.Value.AssemblyPath, declaringType.Value.FullName, methodName);
         }
     }
 }
