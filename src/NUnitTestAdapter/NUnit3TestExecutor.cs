f08d30dc0ec1034b47740f878e2a16f7bde960de
Terje Sandstrom <terje@hermit.no>
2018-12-17 14:43:25 +0100

    FIxed comment

--- src/NUnitTestAdapter/NUnit3TestExecutor.cs
+++ src/NUnitTestAdapter/NUnit3TestExecutor.cs
@@ -337,7 +337,7 @@ namespace NUnit.VisualStudio.TestAdapter
                 return;
 #if NETCOREAPP1_0
 #else
-            var path = Path.Combine(Settings.TestOutputXml, $"{Path.GetFileNameWithoutExtension(assemblyPath)}.xml");
+            var path = Path.Combine(Settings.TestOutputXml, Path.ChangeExtension(Path.GetFileName(assemblyPath),".xml"));
             var resultService = TestEngine.Services.GetService<IResultService>();
             // Following null argument should work for nunit3 format. Empty array is OK as well.
             // If you decide to handle other formats in the runsettings, it needs more work.
