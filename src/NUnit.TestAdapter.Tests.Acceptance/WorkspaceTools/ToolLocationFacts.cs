ef86547c546862c0aba11251423d3cd23116a5d5
jnm2 <me@jnm2.com>
2019-02-16 22:40:00 -0500

    Fall back to prerelease installations of Visual Studio

--- src/NUnit.TestAdapter.Tests.Acceptance/WorkspaceTools/ToolLocationFacts.cs
+++ src/NUnit.TestAdapter.Tests.Acceptance/WorkspaceTools/ToolLocationFacts.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace NUnit.VisualStudio.TestAdapter.Tests.Acceptance.WorkspaceTools
 {
@@ -16,13 +17,9 @@ namespace NUnit.VisualStudio.TestAdapter.Tests.Acceptance.WorkspaceTools
 
         private static readonly Lazy<string> msBuild = new Lazy<string>(() =>
         {
-            var vsInstallation = ProcessUtils.Run(
-                Environment.CurrentDirectory,
-                VSWhere,
-                new[] { "-latest", "-products", "*", "-requires", "Microsoft.Component.MSBuild", "-property", "installationPath" });
-
-            if (string.IsNullOrWhiteSpace(vsInstallation))
-                throw new InvalidOperationException("MSBuild is not installed with Visual Studio on this machine.");
+            var vsInstallation =
+                FindVisualStudio(requiredComponent: "Microsoft.Component.MSBuild")
+                ?? throw new InvalidOperationException("MSBuild is not installed with Visual Studio on this machine.");
 
             return Path.Combine(vsInstallation, @"MSBuild\15.0\Bin\MSBuild.exe");
         });
@@ -31,18 +28,36 @@ namespace NUnit.VisualStudio.TestAdapter.Tests.Acceptance.WorkspaceTools
 
         private static readonly Lazy<string> vsTest = new Lazy<string>(() =>
         {
-            var vsInstallation = ProcessUtils.Run(
-                Environment.CurrentDirectory,
-                VSWhere,
-                // https://github.com/Microsoft/vswhere/issues/126#issuecomment-360542783
-                new[] { "-latest", "-products", "*", "-requires", "Microsoft.VisualStudio.TestTools.TestPlatform.V1.CLI", "-property", "installationPath" });
-
-            if (string.IsNullOrWhiteSpace(vsInstallation))
-                throw new InvalidOperationException("VSTest is not installed with Visual Studio on this machine.");
+            var vsInstallation =
+                FindVisualStudio(requiredComponent: "Microsoft.VisualStudio.TestTools.TestPlatform.V1.CLI") // https://github.com/Microsoft/vswhere/issues/126#issuecomment-360542783
+                ?? throw new InvalidOperationException("VSTest is not installed with Visual Studio on this machine.");
 
             return Path.Combine(vsInstallation, @"Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe");
         });
 
         public static string VSTest => vsTest.Value;
+
+        private static string FindVisualStudio(string requiredComponent)
+        {
+            var arguments = new[] { "-latest", "-products", "*", "-requires", requiredComponent, "-property", "installationPath" };
+
+            var releaseInstallationPath = ProcessUtils.Run(
+                Environment.CurrentDirectory,
+                VSWhere,
+                arguments);
+
+            if (!string.IsNullOrEmpty(releaseInstallationPath))
+                return releaseInstallationPath;
+
+            var prereleaseInstallationPath = ProcessUtils.Run(
+                Environment.CurrentDirectory,
+                VSWhere,
+                arguments.Concat(new[] { "-prerelease" }));
+
+            if (!string.IsNullOrEmpty(prereleaseInstallationPath))
+                return prereleaseInstallationPath;
+
+            return null;
+        }
     }
 }
