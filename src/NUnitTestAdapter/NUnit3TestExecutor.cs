04077963732391546db980c2ca7f520a499306ef
Terje Sandstrom <terje.sandstrom.personal@outlook.com>
2020-01-07 19:42:45 +0100

    Fixes

--- src/NUnitTestAdapter/NUnit3TestExecutor.cs
+++ src/NUnitTestAdapter/NUnit3TestExecutor.cs
@@ -21,7 +21,7 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
- // #define LAUNCHDEBUGGER
+// #define LAUNCHDEBUGGER
 
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
