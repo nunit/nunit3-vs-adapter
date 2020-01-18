321701e7b35b34f456b1214b174387e4c230d0f5
Terje Sandstrom <terje.sandstrom.personal@outlook.com>
2020-01-18 13:00:05 +0100

    Prepare 3.16.1

--- src/NUnitTestAdapter/NUnitTestFilterBuilder.cs
+++ src/NUnitTestAdapter/NUnitTestFilterBuilder.cs
@@ -1,8 +1,28 @@
-﻿using System;
+﻿// ***********************************************************************
+// Copyright (c) 2011-2020 Charlie Poole, Terje Sandstrom
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ***********************************************************************
+
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using NUnit.Engine;
 
@@ -16,10 +36,7 @@ namespace NUnit.VisualStudio.TestAdapter
 
         public NUnitTestFilterBuilder(ITestFilterService filterService)
         {
-            if (filterService == null)
-                throw new NUnitEngineException("TestFilterService is not available. Engine in use is incorrect version.");
-
-            _filterService = filterService;
+            _filterService = filterService ?? throw new NUnitEngineException("TestFilterService is not available. Engine in use is incorrect version.");
         }
 
         public TestFilter ConvertTfsFilterToNUnitFilter(ITfsTestFilter tfsFilter, IList<TestCase> loadedTestCases)
