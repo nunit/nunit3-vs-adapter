b6d65a558b133490ddaf5165689bbf4c157c46c4
Terje Sandstrom <terje@hermit.no>
2021-11-28 17:13:21 +0100

    Fix issue #914

--- src/NUnitTestAdapter/TestConverter.cs
+++ src/NUnitTestAdapter/TestConverter.cs
@@ -345,33 +345,26 @@ namespace NUnit.VisualStudio.TestAdapter
         /// <returns>attachments to be added to the test, it will be empty if no attachments are found.</returns>
         private AttachmentSet ParseAttachments(INUnitTestEvent resultNode)
         {
-            const string fileUriScheme = "file://";
             var attachmentSet = new AttachmentSet(new Uri(NUnitTestAdapter.ExecutorUri), "Attachments");
 
             foreach (var attachment in resultNode.NUnitAttachments)
             {
-                var path = attachment.FilePath;
                 var description = attachment.Description;
-
-                if (!(string.IsNullOrEmpty(path) || path.StartsWith(fileUriScheme, StringComparison.OrdinalIgnoreCase)))
-                {
-                    path = fileUriScheme + path;
-                }
 
                 try
                 {
                     // We only support absolute paths since we dont lookup working directory here
                     // any problem with path will throw an exception
-                    var fileUri = new Uri(path, UriKind.Absolute);
+                    var fileUri = new Uri(attachment.FilePath, UriKind.Absolute);
                     attachmentSet.Attachments.Add(new UriDataAttachment(fileUri, description));
                 }
                 catch (UriFormatException ex)
                 {
-                    _logger.Warning($"Ignoring attachment with path '{path}' due to problem with path: {ex.Message}");
+                    _logger.Warning($"Ignoring attachment with path '{attachment.FilePath}' due to problem with path: {ex.Message}");
                 }
                 catch (Exception ex)
                 {
-                    _logger.Warning($"Ignoring attachment with path '{path}': {ex.Message}.");
+                    _logger.Warning($"Ignoring attachment with path '{attachment.FilePath}': {ex.Message}.");
                 }
             }
 
