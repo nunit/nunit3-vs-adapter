54262a6f8e17eefb97572461845e2bd594da42c0
Ihor Sychevskyi <arhell333@gmail.com>
2022-10-20 12:12:30 +0300

    update src folder links (#1019)

--- src/NUnitTestAdapter/NUnitEngine/NUnitTestEvent.cs
+++ src/NUnitTestAdapter/NUnitEngine/NUnitTestEvent.cs
@@ -224,9 +224,9 @@ namespace NUnit.VisualStudio.TestAdapter.NUnitEngine
     public class NUnitEventWrongTypeException : Exception
     {
         // For guidelines regarding the creation of new exception types, see
-        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        //    https://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
         // and
-        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //    https://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
 
         public NUnitEventWrongTypeException()
         {
