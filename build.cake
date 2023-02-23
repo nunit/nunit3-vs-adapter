a068ca55b78064ef682de50b37a35d339a37bbef
Terje Sandstrom <terje@hermit.no>
2023-02-23 22:09:37 +0100

    4.4.0-beta.1 release

--- build.cake
+++ build.cake
@@ -13,7 +13,7 @@ var configuration = Argument("configuration", "Release");
 //////////////////////////////////////////////////////////////////////
 
 var version = "4.4.0";
-var modifier = "-beta.1.1";
+var modifier = "-beta.1";
 
 
 var dbgSuffix = configuration.ToLower() == "debug" ? "-dbg" : "";
--- src/NUnitTestAdapter/Properties/AssemblyInfo.cs
+++ src/NUnitTestAdapter/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿// ****************************************************************
-// Copyright (c) 2011-2021 Charlie Poole, 2014-2022 Terje Sandstrom.
+// Copyright (c) 2011-2021 Charlie Poole, 2014-2023 Terje Sandstrom.
 // ****************************************************************
 
 using System;
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A package containing the NUnit3 TestAdapter for Visual Studio 2012 onwards. \n\r\nThe package works with Visual Studio 2012 and newer, dotnet test and VSTest.Console.\r\n")]
 [assembly: AssemblyCompany("NUnit Project")]
 [assembly: AssemblyProduct("NUnit3TestAdapter")]
-[assembly: AssemblyCopyright("Copyright © 2011-2021 Charlie Poole, 2014-2022 Terje Sandstrom")]
+[assembly: AssemblyCopyright("Copyright © 2011-2021 Charlie Poole, 2014-2023 Terje Sandstrom")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("c0aad5e4-b486-49bc-b3e8-31e01be6fefe")]
-[assembly: AssemblyVersion("4.3.1.0")]
-[assembly: AssemblyFileVersion("4.3.1.0")]
+[assembly: AssemblyVersion("4.4.0.0")]
+[assembly: AssemblyFileVersion("4.4.0.0")]
 
 [assembly: InternalsVisibleTo("NUnit.VisualStudio.TestAdapter.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010029b97dea816272cc4ea44cf3cf666f8150d6dfe1274b6c2e6c4d54259b756888ec08ad6dd3ea0f540b30408b948ae5f39cf0c7b210abdec267b367ce1eccab97d5c6c02ee67090827ffd699544fa2add4849b45a1901eac08495bfee0397fba3946ff3912ce0b9a497818e418a77a0c8db4ca1780e7b6f6dd6911395fcc0faba")]
