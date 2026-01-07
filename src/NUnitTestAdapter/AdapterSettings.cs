bcf27a0e2afbd44d5210b5a4b6f2596e756f44e1
Terje Sandstrom <terje@hermit.no>
2026-01-07 20:56:52 +0100

    Prepare version 6.1

--- src/NUnitTestAdapter/AdapterSettings.cs
+++ src/NUnitTestAdapter/AdapterSettings.cs
@@ -1,5 +1,5 @@
 // ***********************************************************************
-// Copyright (c) 2014-2021 Charlie Poole, 2014-2025 Terje Sandstrom
+// Copyright (c) 2014-2021 Charlie Poole, 2014-2026 Terje Sandstrom
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
